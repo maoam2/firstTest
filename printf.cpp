6d9bb5f159094970c6d78e6ccc02a511fb5d8cf4
maoam2 <maoam2@centrum.cz>
2015-01-28 11:11:09 +0100

    aaa

---
+++ printf.cpp
@@ -0,0 +1,25 @@
+// my first string
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+	string mystring;
+	int cislo = 0x01;
+	
+	for (int i = 0; i<64; i++) {
+		
+		mystring = "Iterace(";
+		mystring.append(to_string(i));
+		mystring.append("): ");
+		mystring.append(to_string(cislo));
+		mystring.append("\n");
+	
+		cout << mystring << endl;
+		cislo = cislo << 1;
+		
+	}
+
+  return 0;
+}
